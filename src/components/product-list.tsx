3096ca639e7f78387ae36954b537be6ad658fa0d
Yashi Shah <yashisongara@gmail.com>
2024-02-15 10:43:04 -0500

    (feat): Updated products.json and product list

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,11 +1,24 @@
 "use client";
-import productList from "../../products.json";
 import ProductCard from "./product-card";
-import styles from "../styles/product.module.scss"
+import styles from "../styles/product.module.scss";
+import { useState } from "react";
+import { Product } from "@/models/product";
 
 function ProductList() {
+  const [productList, setProductList] = useState<Product[]>([]);
+  const callAPI = async () => {
+    try {
+      const res = await fetch(`/products.json`);
+      const data = await res.json();
+      console.log("data", data);
+      setProductList(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  callAPI();
   return (
-    <div className={styles['product-list']}>
+    <div className={styles["product-list"]}>
       {productList.map((product) => (
         <ProductCard key={product.id} product={product}></ProductCard>
       ))}
